46b53890f4c817b6811a91a0280b1f52105287a3
Marlon Beltran <mbeltran@marencoventiladores.com>
2022-11-12 15:25:22 -0600

    Greeting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    // content: ["./src/**/*.{html,js}"],
+    content: ["./*.html"],
     theme: {
-    extend: {},
+        container: {
+            center: true,
+            padding: '2rem',
+        },
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '868px', // '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '868px', // '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '868px', // '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+        extend: {},
     },
-    plugins: [],
+    plugins: [
+        // require('@tailwindcss/forms'),
+    ],
 }
