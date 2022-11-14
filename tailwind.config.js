cd05db56c19f9e3df22e1c7b4311741ae0067422
Marlon Beltran <mbeltran@marencoventiladores.com>
2022-11-14 08:43:38 -0600

    Fix font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     // content: ["./src/**/*.{html,js}"],
-    content: ["./*.html"],
+    content: ["./**/*.html"],
     theme: {
+        fontFeatureSettings: {
+            numeric: ['tnum', 'salt', 'ss02']
+        },
+        fontSize: {
+            xs: '0.75rem',
+            sm: '0.875rem',
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '36px',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem'
+        },
         container: {
             center: true,
             padding: '2rem',
@@ -14,18 +29,31 @@ module.exports = {
             'md': '768px',
             // => @media (min-width: 768px) { ... }
 
-            'lg': '868px', // '1024px',
+            'lg': '1024px',
             // => @media (min-width: 1024px) { ... }
 
-            'xl': '868px', // '1280px',
+            'xl': '1024px', // '1280px',
             // => @media (min-width: 1280px) { ... }
 
-            '2xl': '868px', // '1536px',
+            '2xl': '1024px', // '1536px',
             // => @media (min-width: 1536px) { ... }
+        },
+        fontFamily: {
+            'sans': ['ui-sans-serif', 'system-ui'],
+            'serif': ['ui-serif', 'Georgia'],
+            'mono': ['ui-monospace', 'SFMono-Regular'],
+            'display': ['Oswald'],
+            'body': ['"Open Sans"'],
         },
         extend: {},
     },
     plugins: [
-        // require('@tailwindcss/forms'),
+        require('tailwindcss-font-inter')({ // default settings
+            a: -0.0223,
+            b: 0.185,
+            c: -0.1745,
+            baseFontSize: 16,
+            importFontFace: true,
+        }),
     ],
 }
